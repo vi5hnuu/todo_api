d386012ebcd65643f4f31f93c8764a1315633462
vi5hnuu <vishnu.12017539@lpu.in>
2023-04-29 23:16:15 +0530

    empty commit

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors(corsOptions))
+// app.use(cors())
 
 app.use(express.json())
 app.use(cookieParser())
--- config/cors_options.js
+++ config/cors_options.js
@@ -2,7 +2,7 @@ const { allowededOrignins } = require('./alloweder_origins')
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowededOrignins.indexOf(origin) !== -1 || !origin) {
+    if (true || allowededOrignins.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not alloweded by cors.'))
